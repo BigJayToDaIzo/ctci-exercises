774cd831b5e78c509ca4b150d247038ec5b88522
Joseph Myers <josephlmyers@gmail.com>
2019-05-18 10:25:54 -0500

    Began chapter 1 problem 5 stub function.

---
+++ src/ch1/ctci_1_5.java
@@ -0,0 +1,15 @@
+package ch1;
+
+public class ctci_1_5 {
+    /*
+    There are three types of edits that can be performed on strings: insert a
+    character, remove a character, or replace a character.  Given two strings,
+    write a function to check if they are one or zero edits away from each
+    other.
+     */
+    public static boolean oneAway(String str1, String str2)
+    {
+        //Stub function does nothing but return true.
+        return true;
+    }
+}
